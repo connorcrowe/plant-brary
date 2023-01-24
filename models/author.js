6bec9208176aad5dcac7fbde895f7382738544c8
connorcrowe <connor.haz.crowe@gmail.com>
2023-01-23 20:36:52 -0500

    Add ability to edit and remove authors, add page to show author and their plants

--- models/author.js
+++ models/author.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose');
+const plant = require('./plant');
+const Book = require('./plant');
 
 const authorSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
     }
+});
+
+authorSchema.pre('remove', function(next) {
+    plant.find({ author: this.id }, (err, plants) => {
+        if (err) {
+            next(err);
+        } else if (plants.length > 0) {
+            next(new Error('This author still has plants'));
+        } else {
+            next();
+        }
+    });
 });
 
 module.exports = mongoose.model('Author', authorSchema);
